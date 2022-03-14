af4feb610f06943551cf2fb84c22726c3b94141b
Bulbul Ahmed <70557643+bulbul-deploy@users.noreply.github.com>
2022-03-14 14:20:48 +0600

    Initial commit

---
+++ src/main/java/enumExamples/Basic.java
@@ -0,0 +1,26 @@
+package enumExamples;
+
+public class Basic {
+
+    enum Week{
+        MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+
+        Week(){
+            System.out.println("Constructor called "+ this);
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        Week day = Week.MONDAY;
+        System.out.println(day);
+        System.out.println(day.ordinal());
+        System.out.println(day.name());
+        System.out.println(day.toString());
+
+        for(Week w : Week.values()){
+            System.out.println(w);
+        }
+    }
+}
---
+++ src/main/java/oop/collections/Main.java
@@ -0,0 +1,34 @@
+package oop.collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        List<Integer> list2 = new LinkedList<>();
+
+
+        list2.add(1);
+        list2.add(2);
+        list2.add(3);
+        list2.add(4);
+
+        System.out.println(list2);
+
+
+        List<Integer> vector = new Vector<>();
+        vector.add(1);
+        vector.add(2);
+        vector.add(3);
+        vector.add(4);
+
+
+        System.out.println(vector);
+
+
+
+    }
+}
